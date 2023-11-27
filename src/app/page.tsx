ce8b40b190477dc8a27597eeb257ffdca19b5bd0
Mario Lisbona <101855037+MarioLisbona@users.noreply.github.com>
2023-11-27 16:31:35 +1100

    use theme in provider and test on home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,5 @@
+import { Text } from "@chakra-ui/react";
+
 export default function Home() {
-  return <main>Home</main>;
+  return <Text color={"link"}>This is some text testing the theme</Text>;
 }
