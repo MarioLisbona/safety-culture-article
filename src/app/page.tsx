b2d277ef455f1f148b7f329234632ac5d82ba5cc
Mario Lisbona <101855037+MarioLisbona@users.noreply.github.com>
2023-11-28 09:57:46 +1100

    use SectionContainer in Page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,18 @@
 import { Button, Text } from "@chakra-ui/react";
+import SectionContainer from "./components/SectionContainer";
 
 export default function Home() {
   return (
     <>
-      <Text textStyle={"heading"} color={"link"}>
-        This is some text testing the theme as a heading
-      </Text>
-      <Text textStyle={"body"} color={"link"}>
-        This is some text testing the theme as a the body
-      </Text>
-      <Button variant={"navBtnClear"}>Click me</Button>
+      <SectionContainer>
+        <Text textStyle={"heading"} color={"link"}>
+          This is some text testing the theme as a heading
+        </Text>
+        <Text textStyle={"body"} color={"link"}>
+          This is some text testing the theme as a the body
+        </Text>
+        <Button variant={"navBtnClear"}>Click me</Button>
+      </SectionContainer>
     </>
   );
 }
