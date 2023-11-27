bd0c3b181d490c0bcc725173a29eae100e29bf64
Mario Lisbona <101855037+MarioLisbona@users.noreply.github.com>
2023-11-27 16:28:12 +1100

    Create Prodivder and theme

---
+++ theme.ts
@@ -0,0 +1,13 @@
+import { extendTheme } from "@chakra-ui/react";
+
+const theme = extendTheme({
+  colors: {
+    link: "#6559FF",
+    text: "#293745",
+    grey: "#F3F6FB",
+    purple: "#F0F0FF",
+    white: "#FFFFFF",
+  },
+});
+
+export default theme;
