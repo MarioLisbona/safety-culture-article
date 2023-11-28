d808377b2db797b1cddb06be7cb5b6d068fd4bd8
Mario Lisbona <101855037+MarioLisbona@users.noreply.github.com>
2023-11-28 13:25:22 +1100

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,18 +6,6 @@ export default function Home() {
   return (
     <>
       <Article />
-      {/* <SectionContainer>
-        <Text textStyle={"heading"}>
-          This is some text testing the theme as a heading
-        </Text>
-        <Text textStyle={"body"}>
-          This is some text testing the theme as a the body
-        </Text>
-        <Text textStyle={"link"}>
-          This is some text testing the theme as a the body
-        </Text>
-        <Button variant={"navBtnClear"}>Click me</Button>
-      </SectionContainer> */}
     </>
   );
 }
