3160a76411b72ce9cb0e10880a76d7b3919b7590
Mario Lisbona <101855037+MarioLisbona@users.noreply.github.com>
2023-11-28 12:18:11 +1100

    Add icons to buttons

--- src/app/components/MoreHelp.tsx
+++ src/app/components/MoreHelp.tsx
@@ -2,6 +2,11 @@ import React from "react";
 import CardComponent from "./CardComponent";
 import { Button, Flex, Text } from "@chakra-ui/react";
 
+import { BsChatLeft } from "react-icons/bs";
+import { MdOutlineMailOutline } from "react-icons/md";
+import { FiPhone } from "react-icons/fi";
+import { GoPeople } from "react-icons/go";
+
 export default function MoreHelp() {
   return (
     <CardComponent border="2px solid" borderColor="link">
@@ -10,10 +15,30 @@ export default function MoreHelp() {
           <Text textStyle={"smHeading"}>Need More Help?</Text>
         </Flex>
         <Flex w={"100%"} justify={"flex-start"} align={"center"}>
-          <Button variant={"cardBtnPurple"}>Live Chat</Button>
-          <Button variant={"cardBtnPurple"}>Email</Button>
-          <Button variant={"cardBtnPurple"}>Phone</Button>
-          <Button variant={"cardBtnClear"}>Ask the community</Button>
+          <Flex justify={"center"} align={"center"}>
+            <Button variant={"cardBtnPurple"}>
+              <BsChatLeft />
+              <Text ms={2}>Live Chat</Text>
+            </Button>
+          </Flex>
+          <Flex justify={"center"} align={"center"}>
+            <Button variant={"cardBtnPurple"}>
+              <MdOutlineMailOutline />
+              <Text ms={2}>Email</Text>
+            </Button>
+          </Flex>
+          <Flex justify={"center"} align={"center"}>
+            <Button variant={"cardBtnPurple"}>
+              <FiPhone />
+              <Text ms={2}>Phone</Text>
+            </Button>
+          </Flex>
+          <Flex justify={"center"} align={"center"}>
+            <Button variant={"cardBtnClear"}>
+              <GoPeople />
+              <Text ms={2}>Ask the community</Text>
+            </Button>
+          </Flex>
         </Flex>
       </Flex>
     </CardComponent>
