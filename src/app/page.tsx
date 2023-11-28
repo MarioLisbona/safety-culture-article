041cabbd220b19400342364f62e15d0cf7e3fd57
Mario Lisbona <101855037+MarioLisbona@users.noreply.github.com>
2023-11-28 14:50:39 +1100

    start mapping over JSON

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,19 @@
 import { Button, Text } from "@chakra-ui/react";
 import SectionContainer from "./components/SectionContainer";
 import Article from "./components/Article";
+import { data } from "./data/articleData";
+import { Content } from "next/font/google";
+
+const fields = data.pageProps.items.content.map((item) => {
+  return item.fields.body?.content.map((item) => {
+    item.content.map((content) => {
+      content.nodeType;
+    });
+  });
+});
 
 export default function Home() {
+  console.log("fields", fields);
   return (
     <>
       <Article />
