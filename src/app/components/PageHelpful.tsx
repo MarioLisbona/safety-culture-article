ec3b7919a33de807581b926376d5265c2cac7eca
Mario Lisbona <101855037+MarioLisbona@users.noreply.github.com>
2023-11-28 14:19:23 +1100

    Update PageHelpful.tsx

--- src/app/components/PageHelpful.tsx
+++ src/app/components/PageHelpful.tsx
@@ -7,17 +7,24 @@ import Link from "next/link";
 
 export default function PageHelpful() {
   return (
-    <CardComponent bg="purple" w={{ base: "75vw", md: "75%" }}>
-      <Flex justify={"center"} align={"center"}>
-        <Text textStyle={"body"}>{`Was this page helpful? {US}`}</Text>
+    <CardComponent bg="purple" w={{ base: "100vw", md: "75%" }}>
+      <Flex
+        justify={"center"}
+        align={"center"}
+        direction={{ base: "column", xl: "row" }}
+      >
+        <Text
+          textStyle={"body"}
+          mb={{ base: 2, xl: 0 }}
+        >{`Was this page helpful? {US}`}</Text>
         <Link href={"#"}>
-          <Button variant={"navBtnWhite"}>
+          <Button variant={"navBtnWhite"} mb={{ base: 2, xl: 0 }}>
             <MdOutlineThumbUpOffAlt />
             <Text px={1} textStyle={"body"} color={"link"}>{`Yes {US}`}</Text>
           </Button>
         </Link>
         <Link href={"#"}>
-          <Button variant={"navBtnWhite"}>
+          <Button variant={"navBtnWhite"} mb={{ base: 2, xl: 0 }}>
             <Text px={1} textStyle={"body"} color={"link"}>{`No {US}`}</Text>
             <MdOutlineThumbDownOffAlt />
           </Button>
