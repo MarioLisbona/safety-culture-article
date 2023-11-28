1c44c3f2b006c1f7434a931a2077ded2944e0d35
Mario Lisbona <101855037+MarioLisbona@users.noreply.github.com>
2023-11-28 11:21:21 +1100

    Add apple and google play section

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -1,5 +1,4 @@
-import { Flex } from "@chakra-ui/react";
-import React from "react";
+import { Flex, Image } from "@chakra-ui/react";
 
 export default function Footer() {
   return (
@@ -10,9 +9,34 @@ export default function Footer() {
         justify={"center"}
         align={"center"}
         borderTop={"1px solid gray"}
-        bg={"grey"}
       >
-        <Flex></Flex>
+        <Flex
+          justify={"center"}
+          align={"center"}
+          border={"1px solid black"}
+          borderRadius={12}
+          px={4}
+          py={6}
+        >
+          <Image
+            src={"logos/sc-logo-color.svg"}
+            alt="SafetyCulture Logo"
+            h={"30px"}
+            px={2}
+          />
+          <Image
+            src={"logos/app-store.svg"}
+            alt="SafetyCulture Logo"
+            h={"30px"}
+            px={2}
+          />
+          <Image
+            src={"logos/google-play-store.svg"}
+            alt="SafetyCulture Logo"
+            h={"30px"}
+            px={2}
+          />
+        </Flex>
       </Flex>
       <Flex
         w={"100%"}
